0c6358fb6b45a2bd640e7115082daf119c1dc7e3
BeaGuti <beatriz.gutierrez@gmail.com>
2024-05-10 19:22:43 +0200

    rename get max code or test per context window functions

--- explain_pr/adapters/adjusted_pull_request_for_llm.py
+++ explain_pr/adapters/adjusted_pull_request_for_llm.py
@@ -2,18 +2,17 @@ from explain_pr.providers.github.pull_request_data import PullRequestData
 from explain_pr.providers.github.pull_request_analytics import PullRequestAnalytics
 from explain_pr.providers.chatgpt.chatggp_provider import MAX_TOKENS
 
-def get_max_code_per_context_window(n_tokens: int = MAX_TOKENS) -> int:
+def get_max_code_chars_per_context_window(n_tokens: int = MAX_TOKENS) -> int:
     # - 1 token is 2.5 characters for diffs and code
     return n_tokens * 2.5
 
 
-def get_max_text_per_context_window(n_tokens: int = MAX_TOKENS) -> int:
+def get_max_text_chars_per_context_window(n_tokens: int = MAX_TOKENS) -> int:
     # - 1 token is 4 characters for English text
     return n_tokens * 4
 
 
-def get_remaining_tokens_per_context_window(chars_used: int) -> int:
-  def get_remaining_tokens_per_context_window(chars_used: int, chars_per_token: int) -> int:
+def get_remaining_tokens_per_context_window(chars_used: int, chars_per_token: int) -> int:
       return MAX_TOKENS - (chars_used / chars_per_token)
 
 
@@ -27,7 +26,7 @@ def adjust_patch_data_size(
     )
     size_per_code = sum([file["total_size"] for file in pr_analytics.file_changes_size])
 
-    remain_tokens = get_remaining_tokens_per_context_window(size_per_text)  
+    remain_tokens = get_remaining_tokens_per_context_window(size_per_text, 4)
     # if PR so big, leave only title
     if remain_tokens <= 0:
         remain_tokens = 0
@@ -37,7 +36,7 @@ def adjust_patch_data_size(
         print("> Adjusting patch data size -> removing description, commit messages and file changes.")
         return pr_data
 
-    remain_code_size = get_max_code_per_context_window(remain_tokens)
+    remain_code_size = get_max_code_chars_per_context_window(remain_tokens)
     # sorted_pr_analytics = sorted(
     #     pr_analytics.file_changes_size, key=lambda x: x["total_size"], reverse=True
     # )
@@ -50,5 +49,5 @@ def adjust_patch_data_size(
             size_per_code -= file_change_size["total_size"]
             if size_per_code < remain_code_size:
                 return pr_data
-           
+
     return pr_data
