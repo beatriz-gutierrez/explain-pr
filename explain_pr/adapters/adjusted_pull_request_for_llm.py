e110542055f9c9be94fb3e6bb8f668505c9fc807
BeaGuti <beatriz.gutierrez@gmail.com>
2024-05-10 19:27:10 +0200

    replate remain by remaining

--- explain_pr/adapters/adjusted_pull_request_for_llm.py
+++ explain_pr/adapters/adjusted_pull_request_for_llm.py
@@ -26,28 +26,28 @@ def adjust_patch_data_size(
     )
     size_per_code = sum([file["total_size"] for file in pr_analytics.file_changes_size])
 
-    remain_tokens = get_remaining_tokens_per_context_window(size_per_text, 4)
+    remaining_tokens = get_remaining_tokens_per_context_window(size_per_text, 4)
     # if PR so big, leave only title
-    if remain_tokens <= 0:
-        remain_tokens = 0
+    if remaining_tokens <= 0:
+        remaining_tokens = 0
         pr_data.description = ""
         pr_data.commit_messages = []
         pr_data.file_changes = []
         print("> Adjusting patch data size -> removing description, commit messages and file changes.")
         return pr_data
 
-    remain_code_size = get_max_code_chars_per_context_window(remain_tokens)
+    remaining_code_size = get_max_code_chars_per_context_window(remaining_tokens)
     # sorted_pr_analytics = sorted(
     #     pr_analytics.file_changes_size, key=lambda x: x["total_size"], reverse=True
     # )
-    if size_per_code > remain_code_size:
+    if size_per_code > remaining_code_size:
         # order by bigger size and remove until fits
         for file_change, file_change_size in zip(pr_data.file_changes, pr_analytics.file_changes_size):
             print("> Adjusting patch data size -> removing 'total_size' in file changes.")
 
             pr_data.file_changes[file_change]['changes_patch'] = ''
             size_per_code -= file_change_size["total_size"]
-            if size_per_code < remain_code_size:
+            if size_per_code < remaining_code_size:
                 return pr_data
 
     return pr_data
