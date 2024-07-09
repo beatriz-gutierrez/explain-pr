5ca166625e1cdb3f9aff374025a4f3cb4333f147
Beatriz <beatriz.gutierrez@gmail.com>
2024-07-09 12:32:13 +0200

    add docstrings with the format of the PullResquestData object

--- explain_pr/providers/github/pull_request_data.py
+++ explain_pr/providers/github/pull_request_data.py
@@ -2,22 +2,48 @@ from dataclasses import dataclass
 from typing import (List, Dict, Union)
 
 
+"""
+Format of PullRequestData:
+{
+    'title': 'Update README.md',
+    'description': 'Some description',
+    'commit_messages': {
+        'commit_id1': 'commit_message1',
+        'commit_id2': 'commit_message2',
+        ...
+    },
+    'file_changes': {
+        'commit_id1': [
+            {
+                'filename': 'README.md',
+                'status': 'modified',
+                'changes_patch': "----",
+                'count_additions': 0,
+                'count_deletions': 1,
+                'count_changes': 1
+            },
+            ...
+        ],
+        'commit_id2': [
+            {
+                'filename': 'README.md',
+                'status': 'modified',
+                'changes_patch': "----",
+                'count_additions': 0,
+                'count_deletions': 1,
+                'count_changes': 1
+            },
+            ...
+        ],
+        ...
+    }
+}
+
+"""
 @dataclass
 class PullRequestData:
     title: str
     description: str
    
-    commit_messages: Dict[str, str] # key: commit_id and value: commit_message
-    file_changes: Dict[str, List[Dict[str, Union[str, int]]]] # key1: commit_id
-    # format of file_changes=
-    #   [
-    #       {
-    #           filename': 'README.md',
-    #           'status': 'modified',
-    #           'changes_patch': "----',
-    #           'count_additions': 0, 
-    #           'count_deletions': 1, 
-    #           'count_changes': 1
-    #       },
-    #      ...
-    #   ]
+    commit_messages: Dict[str, str] 
+    file_changes: Dict[str, List[Dict[str, Union[str, int]]]]
