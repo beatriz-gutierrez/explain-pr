d5a1e2ad0eb7275747e8404c94f6ea03be86ce6f
Beatriz <beatriz.gutierrez@gmail.com>
2024-07-09 12:36:46 +0200

    add docstrings with the format of the PullResquestAnalytics object

--- explain_pr/providers/github/pull_request_analytics.py
+++ explain_pr/providers/github/pull_request_analytics.py
@@ -1,25 +1,47 @@
 from dataclasses import dataclass
 from typing import (List, Dict)
 
+"""
+Format of PullRequestAnalytics:
+{
+    'title_size': 16,
+    'description_size': 13,
+    'commit_messages_size': {
+        'commit_id1': 13,
+        'commit_id2': 13,
+        ...
+    },
+    'file_changes_size': {
+        'commit_id1': [
+            {
+                'filename': 'README.md',
+                'filename_size': 9,
+                'status_size': 8,
+                'changes_patch_size': 524,
+                'total_size': 5 n n nn41
+            },
+            ...
+        ],
+        'commit_id2': [
+            {
+                'filename': 'README.md',
+                'filename_size': 9,
+                'status_size': 8,
+                'changes_patch_size': 524,
+                'total_size': 5 n n nn41
+            },
+            ...
+        ],
+        ...
+    }
+}
 
+
+"""
 @dataclass
 class PullRequestAnalytics:
     title_size: int
     description_size: int
 
-    commit_messages_size: Dict[str, int]  # key: commit_id and value: commit_size
-    file_changes_size: Dict[str, List[Dict[str, int]]] # key1: commit_id
-
-    #  format of file_changes_size=
-    #       [
-    #           {
-    #               'filename': 'README.md',
-    #               'filename_size': 9,
-    #               'status_size': 8,
-    #               'changes_patch_size': 524,
-    #               'total_size': 5 n n nn41
-    #           },
-    #           ...
-    #       ]
-
-
+    commit_messages_size: Dict[str, int] 
+    file_changes_size: Dict[str, List[Dict[str, int]]] 
