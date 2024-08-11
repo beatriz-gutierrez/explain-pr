ef240733a3f370ba8e9bb093bce1c507e3a821d6
Kartones <diego@kartones.net>
2024-08-11 20:05:26 +0200

    fix: properly format markdown blocks

--- explain_pr/providers/chatgpt/chatggp_provider.py
+++ explain_pr/providers/chatgpt/chatggp_provider.py
@@ -3,6 +3,7 @@ import openai
 MAX_TOKENS = 128 * 1000
 SUMMARY_WORDS = 500
 
+
 def get_pull_request_summary(pull_request_content: str) -> str:
     print("> Summarizing pull request")
 
@@ -11,9 +12,10 @@ def get_pull_request_summary(pull_request_content: str) -> str:
 
     prompt = f"""
     Generate a summary of at maximum {SUMMARY_WORDS} words from the following pull request, delimited by triple @ symbols.
-    Use a technical language, and first add a summary of the content in 3 lines and then explain each change in the pull 
+    The format of the summary will be standard markdown.
+    Use a technical language, and first add a summary of the content in 3 lines and then explain each change in the pull
     request using bullet points. Then, explore issues in the code such as errors, bugs, or potential improvements.
-    In case of errors or bugs, add the code that implements the fix delimited by triple '''.
+    In case of errors or bugs, add the code that implements the fix delimited by triple backticks. Do NOT include any language specifier when specifying backtick markdown blocks.
     Finally, add a conclusion with the impact of the changes in the codebase.
 
     Content: @@@{pull_request_content}@@@
