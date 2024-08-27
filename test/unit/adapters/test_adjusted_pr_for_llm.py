a63eae09027021434567e471dc2f081bf522cb03
BeaGuti <beatriz.gutierrez@gmail.com>
2024-08-27 20:20:13 +0200

    unit tests in progress

--- test/unit/adapters/test_adjusted_pr_for_llm.py
+++ test/unit/adapters/test_adjusted_pr_for_llm.py
@@ -1,5 +1,5 @@
 import pytest
-from unittest.mock import patch, MagicMock
+from unittest.mock import MagicMock
 from explain_pr.providers.github.pull_request_data import PullRequestData
 from explain_pr.providers.github.pull_request_analytics import PullRequestAnalytics
 from explain_pr.adapters.adjusted_pull_request_for_llm import adjust_patch_data_size
@@ -19,15 +19,15 @@ class TestAdjustedPullRequestForLlm:
             "mock_file_sha_1": {
                 "filename": "Mock File 1",
                 "status": "modified",
-                "changes_patch": "----",
+                "changes_patch": "--------------------",
                 "count_additions": 0,
                 "count_deletions": 1,
                 "count_changes": 1,
             },
             "mock_file_sha_2": {
-                "filename": "Mock File 2",
+                "filename": "Mock File 222",
                 "status": "modified",
-                "changes_patch": "----",
+                "changes_patch": "----------------------------------------",
                 "count_additions": 0,
                 "count_deletions": 1,
                 "count_changes": 1,
@@ -35,37 +35,35 @@ class TestAdjustedPullRequestForLlm:
         }
 
         self.mock_pull_request_analytics = MagicMock(spec=PullRequestAnalytics)
-        self.mock_pull_request_analytics.title_size = 16
-        self.mock_pull_request_analytics.description_size = 38
+        self.mock_pull_request_analytics.title_size = len(self.mock_pull_request_data.title)
+        self.mock_pull_request_analytics.description_size = len(self.mock_pull_request_data.description)
         self.mock_pull_request_analytics.commit_messages_size = {
-            "mock_commit_sha_1": 13,
-            "mock_commit_sha_2": 24,
+            "mock_commit_sha_1": 21,
+            "mock_commit_sha_2": 21,
         }
         self.mock_pull_request_analytics.files_changes_size = {
             "mock_file_sha_1": {
-                "filename_size": 9,
+                "filename_size": 11,
                 "status_size": 8,
-                "changes_patch_size": 5255554,
-                "total_size": 500000000,
+                "changes_patch_size": 20,
+                "total_size": 39,
             },
             "mock_file_sha_2": {
                 "filename_size": 13,
-                "status_size": 2,
-                "changes_patch_size": 66666661444,
-                "total_size": 200000000000,
+                "status_size": 8,
+                "changes_patch_size": 40,
+                "total_size": 61,
             },
         }
 
     @pytest.fixture(autouse=True)
     def teardown_method(self):
-        # clear any call history and reset any return values or side effects
-        # set during the test
         self.mock_pull_request_data.reset_mock()
         self.mock_pull_request_analytics.reset_mock()
 
     def test_adjust_patch_data_size_with_pr_smaller_than_limit(self):
         # ARRANGE
-        max_tokens = 10000
+        max_tokens = 100000
 
         # ACT
         result = adjust_patch_data_size(
@@ -73,32 +71,42 @@ class TestAdjustedPullRequestForLlm:
         )
 
         # ASSERT
-        assert result is self.mock_pull_request_data
+        assert (
+            len(
+                [
+                    file
+                    for file in result.files_changes.values()
+                    if file["changes_patch"] == ""
+                ]
+            )
+            == 0
+        )
 
-    def test_adjust_patch_data_size_with_pr_bigger_than_limit(self):
+    def test_adjust_patch_data_size_with_pr_bigger_than_limit_than_skip_all_changes(self):
         # ARRANGE
-        max_tokens = 1
-        mock_changes_path_ocurrences = len(
-            [
-                file
-                for file in self.mock_pull_request_data.files_changes.values() if file["changes_patch"]!="" 
-            ]
-        )
+        max_tokens = 10
 
         # ACT
         result = adjust_patch_data_size(
             self.mock_pull_request_data, self.mock_pull_request_analytics, max_tokens
         )
-        print("mock_changes_path_ocurrences:", mock_changes_path_ocurrences)
-        print("result.files_changes:", result.files_changes)
-        print("Non-empty changes_patch count in result:", len([file for file in result.files_changes.values() if file["changes_patch"]!=""]))
 
         # ASSERT
-        assert result is self.mock_pull_request_data
+        assert result.title == self.mock_pull_request_data.title
+        assert result.description == "" 
+        assert not result.commit_messages 
+        assert not result.files_changes 
+
+    def test_adjust_patch_data_size_with_pr_bigger_than_limit_than_skip_some_changes_patch(self):
+        # ARRANGE
+        max_tokens = 50
+
+        # ACT
+        result = adjust_patch_data_size(
+            self.mock_pull_request_data, self.mock_pull_request_analytics, max_tokens
+        )
+        # ASSERT
         assert result.title == self.mock_pull_request_data.title
         assert result.description == self.mock_pull_request_data.description
         assert result.commit_messages == self.mock_pull_request_data.commit_messages
-        assert (
-            len([file for file in result.files_changes.values() if file["changes_patch"]!=""]) 
-            < mock_changes_path_ocurrences
-        )
+        assert len([file for file in result.files_changes.values() if file["changes_patch"]==""]) > 0
--- test/unit/providers/github/test_github_provider.py
+++ test/unit/providers/github/test_github_provider.py
@@ -1,34 +1,92 @@
-# import pytest
-# from unittest.mock import Mock
-# from requests import get
-# import os
-# import sys
+import pytest
+from unittest.mock import MagicMock
+from requests import get
+import os
+import sys
+from explain_pr.providers.github.github_provider import GitHubProvider
+from explain_pr.providers.github.pull_request_data import PullRequestData
 
-# dir = os.path.dirname(__file__)
-# sys.path.append(os.path.join(dir, "..", "..", "..", "..", "explain_pr"))
+class TestGitHubProvider:
 
-# from providers.github.github_provider import GitHubProvider
+    @pytest.fixture(autouse=True)
+    def setup_function(self, mock_url):
+        self.mock_request_get = MagicMock(spec=get)
+
+        self.mock_request_get.url = (
+            mock_url
+        )
+        self.mock_request_get.headers = {}
+        self.mock_request_get.timeout = 0
+        # data = {
+        #     "title": "title", 
+        #     "body": "description", 
+        #     "commit_messages": {
+        #         "commit1_sha": "commit_message1", 
+        #         "commit2_sha": "commit_message2"
+        #     }, 
+        #     "files_changes": {
+        #         "file1_sha": {
+        #             "filename": "README.md", 
+        #             "status": "modified", 
+        #             "changes_patch": "----", 
+        #             "count_additions": 0, 
+        #             "count_deletions": 1, 
+        #             "count_changes": 1
+        #         }, 
+        #         "file2_sha": {
+        #             "filename": ".gitignore", 
+        #             "status": "modified", 
+        #             "changes_patch": "----", 
+        #             "count_additions": 0, 
+        #             "count_deletions": 1, 
+        #             "count_changes": 1
+        #         }
+        #     }
+        # }
+        # self.mock_request_get.json.return_value = PullRequestData(
+        #         title=f"{data['title']}", 
+        #         body=f"{data['body']}", 
+        #         commit_messages=f"{data['commit_messages']}",
+        #         files_changes=f"{data['files_changes']}"
+        # )
+        # analytics = {
+        #     "title_size": len(data["title"]),
+        #     "description_size": len(data["body"]),
+        #     "commit_messages_size": {key: len(value) for key, value in data["commit_messages"].items()},
+        #     "files_changes_size": {}
+        # }
+    
+
+    @pytest.fixture(autouse=True)
+    def teardown_function(self):
+        self.mock_request_get.reset_mock()
 
 
-# # def setup_function():
-# #     pass
+    def test_get_pull_requestr_data_with_valid_pr_number(self):
+        # Arrange
+        git_provider = GitHubProvider()
+        urls = [
+            "https://api.github.com/repos/owner/repository/pulls/1",
+            "https://api.github.com/repos/owner/repository/pulls/1/commits",
+            "https://api.github.com/repos/owner/repository/pulls/1/files",
+        ]
+        self.mock_request_get.side_effect = [MagicMock(url=MagicMock(return_value=url_i)) for url_i in urls]
+        
+        # Act
+        pr_data, pr_analytics = git_provider.get_pull_request_data(
+            "owner", "repository", 1
+        )
 
-# # def teardown_function():
-# #     pass
-
-
-# def mock_requests_get():
-#     mock_get = Mock(spec=get)
-
-#     mock_get.url = "https://api.github.com/repos/owner/repository/pulls/1"
-#     mock_get.headers = {}
-#     mock_get.timeout = 0
-#     data = {"title": "title", "body": "description"}
-#     mock_get.json.return_value = (
-#         f'{{"title": "{data["title"]}", "body": "{data["body"]}"}}'
-#     )
-
-#     return mock_get
+        # Assert
+        assert pr_data.title == self.mock_requests_get.json()["title"]
+        assert pr_data.description == self.mock_requests_get.json()["body"]
+        self.mock_requests_get.assert_called_once_with(
+            self.mock_url,
+            headers=self.mock_requests_get.headers,
+            timeout=self.mock_requests_get.timeout,
+        )
+        assert pr_data.commit_messages == self.mock_requests_get.json()["commit_messages"]
+        assert pr_data.files_changes == self.mock_requests_get.json()["files_changes"]
 
 
 # def test_get_pr_details_with_valid_pr_number(mock_requests_get):
