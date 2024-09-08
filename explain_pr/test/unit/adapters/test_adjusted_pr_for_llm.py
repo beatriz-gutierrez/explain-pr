653c4d62a577ede06acc5e6b6d320a33cd080911
BeaGuti <beatriz.gutierrez@gmail.com>
2024-09-08 18:35:13 +0200

    move test folder inside explain_pr folder

--- explain_pr/test/unit/adapters/test_adjusted_pr_for_llm.py
+++ explain_pr/test/unit/adapters/test_adjusted_pr_for_llm.py
@@ -6,7 +6,7 @@ import sys
 
 # Add the root directory of the project to the Python path
 sys.path.insert(
-    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../"))
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../../"))
 )
 from explain_pr.providers.github.pull_request_data import PullRequestData
 from explain_pr.providers.github.pull_request_analytics import PullRequestAnalytics
